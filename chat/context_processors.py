975b36a113e3264f273a9d62c20ceeee80471277
djangoner <va.efimov@mail.ru>
2021-02-25 16:46:00 +0200

    smallfix

--- chat/context_processors.py
+++ chat/context_processors.py
@@ -4,13 +4,14 @@ def unread_messages(request):
     unread_messages = 0
     is_admin = models.is_admin(request.user)
     #
-    if is_admin:
-        unread_messages = models.ChatMessage.objects.filter(~models.models.Q(author="admin"), readed=False).count()
-    else:
-        matching_chats = models.Chat.objects.filter(user=request.user)
-        if matching_chats:
-            chat = matching_chats.first()
-            unread_messages = chat.get_unread_count(is_admin)
+    if request.user.is_authenticated:
+        if is_admin:
+            unread_messages = models.ChatMessage.objects.filter(~models.models.Q(author="admin"), readed=False).count()
+        else:
+            matching_chats = models.Chat.objects.filter(user=request.user)
+            if matching_chats:
+                chat = matching_chats.first()
+                unread_messages = chat.get_unread_count(is_admin)
     return {
         "unread_messages": unread_messages
     }
