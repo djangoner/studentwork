75b373bd8be576acc445ecce7dedb19ed44bd85e
djangoner <va.efimov@mail.ru>
2021-02-28 15:45:58 +0200

    order work form created

--- main/forms.py
+++ main/forms.py
@@ -13,3 +13,24 @@ class DocumentUploadForm(forms.ModelForm):
         super().__init__(*args, **kwargs)
         for field in self.fields.values():
             field.required = True
+
+class OrderWorkForm(forms.Form):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs['class'] = 'form-control'
+
+    subject     = forms.CharField(max_length=30, required=True, widget=forms.TextInput(attrs={'placeholder': 'Например: Биология'}),
+                        label="Предмет")
+    theme       = forms.CharField(max_length=30, required=True, widget=forms.TextInput(attrs={'placeholder': 'Например: анатомия человека'}),
+                        label="Тема работы")
+    type        = forms.ChoiceField(choices=models.DOCUMENT_TYPES, required=True,
+                        label="Тип работы")
+    pages_count = forms.IntegerField(max_value=100, min_value=1, required=True,
+                        label="Количество страниц")
+    deadline    = forms.DateField(required=True,
+                        label="Срок")
+    email       = forms.EmailField(required=True, max_length=60, widget=forms.TextInput(attrs={'placeholder': 'user@gmail.com'}),
+                        label="Ваш e-mail")
+    comment     = forms.CharField(max_length=500, widget=forms.Textarea(attrs={"rows":2, 'placeholder': 'Комментарий...'}), required=False,
+                        label="Комментарий к работе")
